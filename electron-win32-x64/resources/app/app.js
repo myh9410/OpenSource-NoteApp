311b41789e9863393fad16d8a7e2f70ccd4c26be
myh9410 <mjskks94@naver.com>
2017-05-01 23:12:39 +0900

    note-app packaging

---
+++ electron-win32-x64/resources/app/app.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var rest_api = require('./routes/rest_api')
+
+app.use(bodyParser.json()); // for parsing application/json
+
+app.use('/', express.static(__dirname + '/'));
+app.use('/ShowMemo', express.static(__dirname + '/'));
+app.use('/Calendar', express.static(__dirname + '/'));
+app.use('/ShowTime', express.static(__dirname + '/'));
+app.use('/WriteMemo', express.static(__dirname + '/'));
+
+
+app.all('/api/add', function (req, res, next) {
+     console.log('API is called...');
+     api = new rest_api();
+     result = api.write(req);
+     if (result) {
+         res.json({ success: true })
+     }
+});
+
+app.all('/api/list', function (req, res, next) {
+    console.log('List API is called...');
+    api = new rest_api();
+    api.list(req, res);
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
---
+++ electron-win32-x64/resources/app/src/opensource_1/OpenSourceDay01.java
@@ -0,0 +1,15 @@
+package opensource_1;
+
+import java.util.Scanner;
+
+public class OpenSourceDay01 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		if (i == 2){
+			System.out.println("Moon Yong Ho");
+			System.out.println("1994/10/21");
+
+		}
+	}
+}
